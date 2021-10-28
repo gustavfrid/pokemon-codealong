8387196641eb390437120480e868f68f8864965f
Gustav Frid <gustav.frid@gmail.com>
2021-10-28 12:34:48 +0200

    fetch pokemons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 export const App = () => {
+  const [pokemons, setPokemons] = useState([])
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon')
+      .then(res => res.json())
+      .then(data => setPokemons(data.results))
+  }, [])
   return (
     <div>
-      Find me in src/app.js!
+      <ul>
+        {pokemons.map(pokemon => (
+          <li key={pokemon.name}>{pokemon.name}</li>
+        ))}
+      </ul>
     </div>
   )
 }
